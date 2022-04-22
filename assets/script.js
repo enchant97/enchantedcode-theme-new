98d9e66f73af96efd51977867307e0167efa578d
Leo Spratt <enchant97@users.noreply.github.com>
2022-04-22 15:03:11 +0100

    add scroll-to-top & hide theme picker until js is enabled

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,43 @@
+function create_element_anchor(parent) {
+    if (!parent.id) { return }
+
+    let anchor = document.createElement("a");
+    anchor.classList.add("page-anchor");
+    anchor.title = "Anchor For: '" + parent.innerText + "'";
+    anchor.href = document.location.href + "#" + parent.id;
+    anchor.innerText = "¶";
+
+    parent.append(anchor);
+}
+
+function add_article_anchors() {
+    let headings = document.querySelectorAll('#main article :is(h1, h2, h3, h4, h5, h6)');
+    headings.forEach(heading => {
+        create_element_anchor(heading);
+    });
+}
+
+function setup_theme_picker_bnt() {
+    let picker_bnt = document.getElementById("theme-toggle-bnt")
+    picker_bnt.addEventListener("click", ThemeChanger.toggle_theme_picker);
+    picker_bnt.classList.remove("hidden");
+}
+
+function setup_back_to_top() {
+    let bnt = document.createElement("a");
+    bnt.id = "to-top-bnt";
+    bnt.innerText = "To Top";
+    bnt.href = "#";
+    document.addEventListener("scroll", _ => {
+        if (window.scrollY > 200) {
+            bnt.style.display = "block";
+        } else {
+            bnt.style.display = "none";
+        }
+    });
+    document.body.append(bnt);
+}
+
 ThemeChanger.themes = {
     os: {
         name: "OS",
@@ -33,7 +73,8 @@ ThemeChanger.themes = {
 
 ThemeChanger.theme_picker_parent = document.querySelector("main");
 ThemeChanger.selected_theme_css_class = "active";
-
-document.getElementById("theme-toggle-bnt").addEventListener("click", ThemeChanger.toggle_theme_picker);
-
 ThemeChanger.on_load();
+
+setup_theme_picker_bnt();
+add_article_anchors();
+setup_back_to_top()
