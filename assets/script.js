9d8b8f45c7cf2438642130ca10ca8655d0056a3a
Leo Spratt <enchant97@users.noreply.github.com>
2022-07-14 11:49:25 +0100

    update to V2 theme picker

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt
+"use-strict";
+
 function create_element_anchor(parent) {
     if (!parent.id) { return }
 
@@ -38,43 +41,15 @@ function setup_back_to_top() {
     document.body.append(bnt);
 }
 
-ThemeChanger.themes = {
-    os: {
-        name: "OS",
-        css: null,
-    },
-    light: {
-        name: "Light",
-        css: [
-            ["color-scheme", "light"],
-            ["--body-bg", "var(--body-bg--light)"],
-            ["--sub-bg", "var(--sub-bg--light)"],
-            ["--sub2-bg", "var(--sub2-bg--light)"],
-            ["--sub3-bg", "var(--sub3-bg--light)"],
-            ["--main-fg", "var(--main-fg--light)"],
-            ["--faded-fg", "var(--faded-fg--light)"],
-            ["--site-color-scheme", "var(--site-color-scheme--light)"],
-        ]
-    },
-    dark: {
-        name: "Dark",
-        css: [
-            ["color-scheme", "dark"],
-            ["--body-bg", "var(--body-bg--dark)"],
-            ["--sub-bg", "var(--sub-bg--dark)"],
-            ["--sub2-bg", "var(--sub2-bg--dark)"],
-            ["--sub3-bg", "var(--sub3-bg--dark)"],
-            ["--main-fg", "var(--main-fg--dark)"],
-            ["--faded-fg", "var(--faded-fg--dark)"],
-            ["--site-color-scheme", "var(--site-color-scheme--dark)"],
-        ]
-    },
-};
-
 ThemeChanger.theme_picker_parent = document.querySelector("main");
+ThemeChanger.use_local = true;
 ThemeChanger.selected_theme_css_class = "active";
-ThemeChanger.on_load();
 
-setup_theme_picker_bnt();
+let themeToggleBnt = document.getElementById("theme-toggle-bnt")
+themeToggleBnt.addEventListener("click", ThemeChanger.toggle_theme_picker);
+themeToggleBnt.classList.remove("hidden");
+
+ThemeChanger.on_load();
 add_article_anchors();
 setup_back_to_top()
+// @license-end
